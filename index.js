c4270f932da3033bcef02e241afbf565518fd116
Paul Frazee <pfrazee@gmail.com>
2018-01-02 14:32:46 -0600

    Bump to webdb@4.0.0 and use validate functions

--- index.js
+++ index.js
@@ -1,13 +1,8 @@
 const WebDB = require('@beaker/webdb')
+const assert = require('assert')
 const LibFritterSocialAPI = require('./lib/social')
 const LibFritterFeedAPI = require('./lib/feed')
 const {normalizeUrl} = require('./lib/util')
-
-const SCHEMAS = {
-  post: require('./schemas/post.json'),
-  profile: require('./schemas/profile.json'),
-  vote: require('./schemas/vote.json')
-}
 
 // exported API
 // =
@@ -41,7 +36,20 @@ function defineTables (db) {
   db.define('profiles', {
     filePattern: '/profile.json',
     index: ['*followUrls'],
-    schema: SCHEMAS.profile,
+    validate (record) {
+      if (record.name) assert(typeof record.name === 'string', 'The .name attribute must be a string')
+      if (record.bio) assert(typeof record.bio === 'string', 'The .bio attribute must be a string')
+      if (record.avatar) assert(typeof record.avatar === 'string', 'The .avatar attribute must be a string')
+      if (record.follows) {
+        assert(Array.isArray(record.follows), 'The .follows attribute must be an array')
+        for (let i = 0; i < record.follows.length; i++) {
+          assert(record.follows[i] && typeof record.follows[i] === 'object', 'Every value in the .follows array must be an object')
+          assert(typeof record.follows[i].url === 'string', 'Every object in the .follows array must include a .url string')
+          if (record.follows[i].name) assert(typeof record.follows[i].name === 'string', 'Every .name in the .follows objects must be a string')
+        }
+      }
+      return true
+    },
     preprocess (record) {
       record.follows = record.follows || []
       record.followUrls = record.follows.map(f => f.url)
@@ -59,13 +67,24 @@ function defineTables (db) {
   db.define('posts', {
     filePattern: '/posts/*.json',
     index: ['createdAt', ':origin+createdAt', 'threadRoot'],
-    schema: SCHEMAS.post
+    validate (record) {
+      assert(typeof record.text === 'string', 'The .text attribute is required and must be a string')
+      assert(typeof record.createdAt === 'number', 'The .createdAt attribute is required and must be a number')
+      if (record.threadRoot) assert(typeof record.threadRoot === 'string', 'The .threadRoot attribute must be a string')
+      if (record.threadParent) assert(typeof record.threadParent === 'string', 'The .threadRoot attribute must be a string')
+      return true
+    }
   })
 
   db.define('votes', {
     filePattern: '/votes/*.json',
     index: ['subject'],
-    schema: SCHEMAS.vote,
+    validate (record) {
+      assert(typeof record.subject === 'string', 'The .subject attribute is required and must be a string')
+      assert(record.vote === -1 || record.vote === 0 || record.vote === 1, 'The .vote attribute is required and must be a number of the value -1, 0, or 1')
+      if (record.createdAt) assert(typeof record.createdAt === 'number', 'The .createdAt attribute must be a number')
+      return true
+    },
     preprocess (record) {
       record.subject = normalizeUrl(record.subject)
     }
